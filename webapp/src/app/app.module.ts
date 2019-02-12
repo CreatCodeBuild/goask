0d9f6cd69054df20cef616425445dc9e7c86d99b
Shihao Xia <sxia@wpi.edu>
2019-02-12 09:33:59 -0800

    feat: backend docker feat: webapp docker (although for dev, better run nativly for debugging) [webapp]: add apollo angular dep
    remove: webapp
    
    [webapp]: added user component
    [webapp]: added user service
    
    allow Option

--- webapp/src/app/app.module.ts
+++ webapp/src/app/app.module.ts
@@ -1,16 +1,50 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ApolloModule, APOLLO_OPTIONS } from 'apollo-angular';
+import { HttpLinkModule, HttpLink } from 'apollo-angular-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 
 import { AppComponent } from './app.component';
+import { environment } from 'src/environments/environment';
+import { UsersComponent } from './users/users.component';
+import { UserComponent } from './user/user.component';
+import { Routes, RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+
+const routes: Routes = [
+  {
+    path: 'users',
+    component: UsersComponent
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent,
+    UserComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ApolloModule,
+    HttpClientModule,
+    HttpLinkModule,
+    RouterModule.forRoot(routes)
   ],
-  providers: [],
+  providers: [
+    {
+      provide: APOLLO_OPTIONS,
+      useFactory(httpLink: HttpLink) {
+        return {
+          cache: new InMemoryCache(),
+          link: httpLink.create({
+            uri: environment.graphQLEndpoint
+          })
+        };
+      },
+      deps: [HttpLink]
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
